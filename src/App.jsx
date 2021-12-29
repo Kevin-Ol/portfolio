e47a91e637c1e158ef51e4fef60472a3811e3cee
Kevin Oliveira <kevin.zero@hotmail.com>
2021-12-29 20:52:14 -0300

    Finaliza navigation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/general.css'
+import './styles/components.css'
+import Navigation from './components/Navigation'
+
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+function Navigation() {
+  return (
+    <nav class="main-nav">
+      <p>Kevin Oliveira</p>
+      <ul>
+        <li><a class="main-nav-link" href="#">Home</a></li>
+        <li><a class="main-nav-link" href="#">Habilidades</a></li>
+        <li><a class="main-nav-link" href="#">Projetos</a></li>
+        <li><a class="main-nav-link" href="#">Contato</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
